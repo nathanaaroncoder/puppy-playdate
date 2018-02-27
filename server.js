6d83585ff3ddf2817d65a05789a64c60f15f5158
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2018-02-27 10:58:53 -0500

    1 dot

--- server.js
+++ server.js
@@ -71,9 +71,9 @@ app.use(passport.session()) // will call the deserializeUser
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, '../client/build/static')))
+	app.use('/static', express.static(path.join(__dirname, './client/build/static')))
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/build/'))
+		res.sendFile(path.join(__dirname, './client/build/'))
 	})
 }
 
