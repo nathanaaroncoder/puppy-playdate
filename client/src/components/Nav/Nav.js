ed20521a249eee6042f12d73acddf85b3b337be3
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2018-02-27 15:17:15 -0500

    uses links from router

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,25 +1,26 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Nav = () =>
   <nav className="navbar navbar-inverse navbar-top">
     <div className="container-fluid">
     <div className="row">
       <div className="col-xs-3 col-s-3 col-md-3 col-lg-3" id="nav-icons">
-        <a href="/matches" className="navbar-brand" title="Matches">
+        <Link to="/matches" className="navbar-brand" title="Matches">
           <i className="icon-dog icon-large"></i>  
-        </a></div>
+        </Link></div>
         <div className="col-xs-3 col-s-3 col-md-3 col-lg-3" id="nav-icons">
-        <a href="/user_profile" className="navbar-brand" title="Profile">
+        <Link to="/user_profile" className="navbar-brand" title="Profile">
           <span className="glyphicon glyphicon-user btn-lg" id="nav-icons"></span>
-        </a></div>
+        </Link></div>
         <div className="col-xs-3 col-s-3 col-md-3 col-lg-3" id="nav-icons">
-        <a href="/messages" className="navbar-brand" title="Messages">
+        <Link to="/messages" className="navbar-brand" title="Messages">
           <span className="glyphicon glyphicon-envelope btn-lg" id="nav-icons"></span>
-        </a></div>
+        </Link></div>
         <div className="col-xs-3 col-s-3 col-md-3 col-lg-3">
-        <a href="/calendar" className="navbar-brand" title="Calendar">
+        <Link to="/calendar" className="navbar-brand" title="Calendar">
           <span className="glyphicon glyphicon-calendar btn-lg" id="nav-icons"></span>
-        </a>
+        </Link>
         </div>
       </div>
     </div>
