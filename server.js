e1d2fd7434fc9c71f86b6a2234884f32b68bfdfb
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2018-02-27 10:49:45 -0500

    added client to dir

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use(passport.session()) // will call the deserializeUser
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, '../build/static')))
+	app.use('/static', express.static(path.join(__dirname, './client/build/static')))
 	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, '../build/'))
 	})
