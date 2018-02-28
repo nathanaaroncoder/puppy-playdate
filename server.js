982e634b7e106363e609ace9aa10aa40715ddd6b
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2018-02-27 19:13:55 -0500

    deploying

--- server.js
+++ server.js
@@ -57,9 +57,9 @@ uploader.use(
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, '../build/static')))
+	app.use('/static', express.static(path.join(__dirname, './client/build/static')))
 	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname, '../build/'))
+		res.sendFile(path.join(__dirname, './client/build/'))
 	})
 }
 
