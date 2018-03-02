08bec1e0f0bb1537eac3a9649f2ff1768e88a2cb
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2018-03-02 14:47:40 -0500

    changing from static to build

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ uploader.use(
 if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
-	app.use('/static', express.static(path.join(__dirname, './client/build/static/')))
+	app.use('/', express.static(path.join(__dirname, './client/build/')))
 	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, './client/build/'))
 	})
