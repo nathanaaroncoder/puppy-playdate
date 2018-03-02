f3076babf0157941c9c24e15b38f6562eca85812
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2018-03-02 15:22:21 -0500

    removed file uploader app.get

--- server.js
+++ server.js
@@ -61,16 +61,16 @@ if (process.env.NODE_ENV === 'production') {
 	console.log('YOU ARE IN THE PRODUCTION ENV')
 	app.use('/', express.static(path.join(__dirname, './client/build/')))
 	app.get('/', (req, res) => {
-		express.static(path.join(__dirname, './client/build/'))
+		res.sendFile(path.join(__dirname, './client/build/'))
 	})
 }
 
 /* Express app ROUTING */
 
 //Route for profile image retrieval
-app.get("/uploads/:id", (req, res) => {
-  res.sendFile(path.join(__dirname, `uploads/${req.params.id}`));
-});
+// app.get("/uploads/:id", (req, res) => {
+//   res.sendFile(path.join(__dirname, `uploads/${req.params.id}`));
+// });
 
 app.use('/auth', require('./auth'))
 
