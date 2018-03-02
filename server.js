a2c2af14f71aeb796ff5ad3851515a001bde6b3a
Nathan Coder <nathancoder1@Nathans-MacBook-Pro.local>
2018-03-02 12:55:54 -0500

    replacing * with / in server

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ if (process.env.NODE_ENV === 'production') {
 	const path = require('path')
 	console.log('YOU ARE IN THE PRODUCTION ENV')
 	app.use('/static', express.static(path.join(__dirname, './client/build/static')))
-	app.get('*', (req, res) => {
+	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, './client/build/'))
 	})
 }
